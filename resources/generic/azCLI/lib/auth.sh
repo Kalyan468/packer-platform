1d117f6a925afcc88509cc4f0ad0ccc4f53085b7
Viresh Doshi <vdoshi@vireshs-mbp.home>
2018-10-16 20:37:23 +0100

    auth file

---
+++ resources/generic/azCLI/lib/auth.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+function az_login() {
+    az login --service-principal -u "${ARM_CLIENT_ID}" --password "${ARM_CLIENT_SECRET}" --tenant "${ARM_TENANT_ID}" > /dev/null
+    az account set -s "${ARM_SUBSCRIPTION_ID}" > /dev/null
+}
